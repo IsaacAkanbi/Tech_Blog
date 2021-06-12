11072502c34d20fff319be6a35d955d174f70513
IsaacAkanbi <isaac.akanbi@hotmail.co.uk>
2021-06-12 09:39:01 -0400

    Updating front views

--- public/js/add-comment.js
+++ public/js/add-comment.js
@@ -1,25 +1,25 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
+    const title = document.querySelector('input[name="title"]').value;
+    const description = document.querySelector('input[name="description"]').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
             title,
-            post_content
+            description
         }),
         headers: {
             'Content-Type': 'application/json'
         }
     });
 
-    if (response.ok) {
-        document.location.replace('/review');
-    } else {
-        alert(response.statusText);
-    }
+    // if (response.ok) {
+    //     document.location.replace('/review');
+    // } else {
+    //     alert(response.statusText);
+    // }
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
